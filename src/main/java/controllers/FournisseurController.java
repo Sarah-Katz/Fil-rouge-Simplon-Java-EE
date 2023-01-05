f30de4b5c690f7b77fd28f31bc429045c529d1ba
Thomas6A <thomas.baudrin@outlook.fr>
2023-01-05 14:43:25 +0100

    push de page disparue

---
+++ src/main/java/controllers/FournisseurController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import accesDonnees.DAO.FournisseurDAO.FournisseurDAOimpl;
+import accesDonnees.DO.FournisseurDO;
+
+@Controller
+@RequestMapping("/interne")
+public class FournisseurController {
+	private final FournisseurDAOimpl FOURDAO = new FournisseurDAOimpl();
+	private final List<FournisseurDO> LISTEFOUR = FOURDAO.findAll();
+
+	@GetMapping
+	public String getFournisseurs(Model model) {	    
+	    model.addAttribute("fournisseurs", LISTEFOUR);
+	    return "interne";
+	}
+
+	@PostMapping
+	public String achatProd(@RequestParam("id") int idfour, RedirectAttributes redirectAttributes) {
+	    redirectAttributes.addAttribute("idfour", idfour);
+	    return "redirect:/achatprod";
+	}
+
+}
