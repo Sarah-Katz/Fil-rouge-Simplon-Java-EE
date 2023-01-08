bd280cfcf3b0453d3992bd711a47fa6fe044bd68
Sarah-Katz <Sarahkatz59@gmail.com>
2023-01-08 19:44:54 +0100

    Ajout bouton vider le panier

---
+++ src/main/java/controllers/ClearPanierController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import accesDonnees.DAO.ClientDAO.ClientDAOimpl;
+import accesDonnees.DAO.CommandeDAO.CommandeDAOimpl;
+import accesDonnees.DAO.ProduitDAO.ProduitDAOimpl;
+import accesDonnees.DO.ClientDO;
+import accesDonnees.DO.CommandeDO;
+import accesDonnees.DO.ProduitDO;
+
+@Controller
+@RequestMapping("/clear")
+public class ClearPanierController {
+	private static final ProduitDAOimpl PRODDAO = new ProduitDAOimpl();
+	private static final CommandeDAOimpl COMMDAO = new CommandeDAOimpl();
+
+	@PostMapping
+	public String clearPanier(Model model) {
+		final ClientDO client = new ClientDAOimpl().findByMail("mail@mail.mail");
+		final CommandeDO commande = client.getPanier().getCommande();
+		List<ProduitDO> listprod = client.getPanier().getCommande().getListeprod();
+		Iterator<ProduitDO> iterator = listprod.iterator();
+		while (iterator.hasNext()) {
+			ProduitDO p = iterator.next();
+			PRODDAO.updateincomm(p.getIdprod(), false);
+			iterator.remove();
+		}
+		COMMDAO.updateProduit(commande.getIdcomm(), listprod);
+		return "panier";
+	}
+}
--- src/main/java/controllers/PanierController.java
+++ src/main/java/controllers/PanierController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
